b12b36b37ab8eaa110c6a548aaa65d25d3b7e472
peterbassem1996 <peter.bassem.1996@gmail.com>
2020-05-25 23:20:30 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+var heading = document.querySelector("h1");
+
+if (randomNumber1 > randomNumber2){
+  heading.textContent = "🚩 Player 1 wins"
+}
+else if (randomNumber2 > randomNumber1){
+  heading.textContent = "Player 2 wins 🚩"
+}
+else{
+  heading.textContent = "Draw!";
+}
